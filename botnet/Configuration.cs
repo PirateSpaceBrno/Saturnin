0276fc87d89e3c2d019e203b70ca1230105c6bcb
Ondřej Kotas <ok@krtkovo.eu>
2018-05-24 01:34:33 +0200

    First version

---
+++ botnet/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace botnet
+{
+    public class Configuration
+    {
+        public const string USERNAME = "+420775478375";
+        public const string SALUTATION = "Saturnine";
+        public const int TIMEOUT = 10;
+        public const bool DEBUG = true;
+    }
+}
---
+++ botnet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+
+namespace botnet
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            var saturnin = new Saturnin();
+            Thread receiver = new Thread(new ThreadStart(saturnin.DoWork));
+            receiver.Start();
+        }
+    }
+
+    public class Saturnin
+    {
+        public void DoWork()
+        {
+            while (true)
+            {
+                SignalCliCommands.Receive();
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
